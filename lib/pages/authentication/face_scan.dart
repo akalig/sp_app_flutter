adb7c35bc3f05db2f8267e474664f51d9ba0f057
Akali Granger <akaligranger@gmail.com>
2023-11-07 17:30:18 +0800

    11-07-23-2

--- lib/pages/authentication/face_scan.dart
+++ lib/pages/authentication/face_scan.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,22 +10,50 @@ class FaceScan extends StatefulWidget {
 }
 
 class _FaceScanState extends State<FaceScan> {
+  late CameraController controller;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.blueAccent,
-      ),
-      routerConfig: GoRouter(
-        routes: [
-          GoRoute(
-            path: '/',
-            name: 'home',
-            // pageBuilder: (context, state) => NoTransitionPage(child: ),
-          ),
-        ],
+    return Scaffold(
+      body: FutureBuilder(
+        future: initializationCamera(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                AspectRatio(
+                  aspectRatio: 2 / 3,
+                  child: CameraPreview(controller),
+                ),
+                AspectRatio(
+                  aspectRatio: 2 / 3,
+                  child: Image.asset(
+                    'lib/images/camera-overlay-conceptcoder.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
+
+  Future<void> initializationCamera() async {
+    var cameras = await availableCameras();
+    controller = CameraController(
+      cameras[EnumCameraDescription.back.index],
+      ResolutionPreset.medium,
+      imageFormatGroup: ImageFormatGroup.yuv420,
+    );
+    await controller.initialize();
+  }
 }
+
+enum EnumCameraDescription { front, back }
